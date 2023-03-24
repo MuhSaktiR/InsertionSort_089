950395a41e8b8750779159a22426ab79328ea25a
MuhSaktiR <seken876@gmail.com>
2023-03-24 15:38:26 +0700

    variabel for dan while

--- Latihan Insertion Sort/Latihan Insertion Sort.cpp
+++ Latihan Insertion Sort/Latihan Insertion Sort.cpp
@@ -32,4 +32,16 @@ void input() {
 void insertionsort() {
 	int temp;
 	int j;
+
+	for (int i = 1; i < n; i++) {
+
+		temp = arr[i];
+
+		j = i - 1;
+
+		while (j >= 0 && arr[j] > temp) {
+
+			arr[j + 1] = arr[j];
+			j--;
+		}
 }
